1202e0c723f73b0c87f9c4fb26f90c36f8f6ad5c
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-12-26 16:49:15 +0900

    create project

---
+++ src/main/java/com/meomulm/MeomulmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.meomulm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeomulmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeomulmBackendApplication.class, args);
+	}
+
+}
