d33581c2230cdcaa441d0d0e9b1770b935fa9020
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2026-01-02 16:23:29 +0900

    chore/update-review-domain-setting1

--- src/main/java/com/meomulm/review/controller/ReviewController.java
+++ src/main/java/com/meomulm/review/controller/ReviewController.java
@@ -1,13 +1,14 @@
 package com.meomulm.review.controller;
 
+import com.meomulm.common.util.AuthUtil;
 import com.meomulm.review.model.dto.AccommodationReview;
+import com.meomulm.review.model.dto.MyReview;
+import com.meomulm.review.model.dto.Review;
+import com.meomulm.review.model.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +18,55 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReviewController {
 
+    private final ReviewService reviewService;
+    private final AuthUtil authUtil;
+
+    /**
+     * 숙소별 리뷰 조회
+     */
     @GetMapping("/accommodationId/{accommodationId}")
-    public ResponseEntity<List<AccommodationReview>> getReviewByAccommodationId (@PathVariable int accommodationId){
+    public ResponseEntity<List<AccommodationReview>> getReviewByAccommodationId(@PathVariable int accommodationId) {
+        List<AccommodationReview> reviews = reviewService.getReviewByAccommodationId(accommodationId);
 
-
+        return ResponseEntity.ok(reviews);
     }
 
+    /**
+     * 내 리뷰 조회
+     */
+    @GetMapping("/userId")
+    public ResponseEntity<List<MyReview>> getReviewByUserId(@RequestHeader(value = "Authorization") String authHeader) {
+        int currentUserId = authUtil.getCurrentUserId(authHeader);
+        List<MyReview> reviews = reviewService.getReviewByUserId(currentUserId);
+
+        return ResponseEntity.ok(reviews);
+    }
+
+    /**
+     * 리뷰 작성
+     */
+    @PostMapping
+    public ResponseEntity<Void> postReview(@RequestHeader(value = "Authorization") String authHeader, @RequestBody Review review) {
+        int currentUserId = authUtil.getCurrentUserId(authHeader);
+
+        reviewService.postReview(
+                currentUserId,
+                review.getAccommodationId(),
+                review.getRating(),
+                review.getReviewContent()
+        );
+
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * 리뷰 삭제
+     */
+    @DeleteMapping("/{reviewId}")
+    public ResponseEntity<Void> deleteReview(@RequestHeader(value = "Authorization") String authHeader, @PathVariable int reviewId) {
+        int currentUserId = authUtil.getCurrentUserId(authHeader);
+        reviewService.deleteReview(reviewId, currentUserId);
+
+        return ResponseEntity.ok().build();
+    }
 }
