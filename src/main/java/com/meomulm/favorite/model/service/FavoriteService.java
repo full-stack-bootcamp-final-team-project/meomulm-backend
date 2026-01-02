1c9adab087272fb7de4ad5a5e38e557b1887bbd6
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2026-01-02 16:43:46 +0900

    favorite model, controller

--- src/main/java/com/meomulm/favorite/model/service/FavoriteService.java
+++ src/main/java/com/meomulm/favorite/model/service/FavoriteService.java
@@ -6,6 +6,6 @@ import java.util.List;
 
 public interface FavoriteService {
     List<SelectFavorite> getFavorites(int userId); // 숙소 Id 하나만 가져와서 String
-    void addFavorite(int userId, int accommodationId);
+    void postFavorite(int userId, int accommodationId);
     void deleteFavorite(int userId, int favoriteId);
 }
