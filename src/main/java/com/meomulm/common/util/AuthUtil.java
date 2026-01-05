279579364bac6f758e6f1fb16c5b29b737b6beaf
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2026-01-05 16:42:26 +0900

    chore/update-review-domain-setting2

--- src/main/java/com/meomulm/common/util/AuthUtil.java
+++ src/main/java/com/meomulm/common/util/AuthUtil.java
@@ -1,5 +1,6 @@
 package com.meomulm.common.util;
 
+import com.meomulm.common.exception.UnauthorizedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +11,15 @@ public class AuthUtil {
     private final JwtUtil jwtUtil;
 
     public int getCurrentUserId(String authHeader) {
+
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            throw new IllegalArgumentException("Invalid Authorization header");
+            throw new UnauthorizedException("로그인이 필요합니다.");
         }
-        return jwtUtil.getUserIdFromToken(authHeader.substring(7));
+
+        try {
+            return jwtUtil.getUserIdFromToken(authHeader.substring(7));
+        } catch (Exception e) {
+            throw new UnauthorizedException("유효하지 않은 토큰입니다.");
+        }
     }
 }
