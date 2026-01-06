1739ffdfa98cb1a4b69c50f011020848b9b544c8
Emma10003 <5emma10003@gmail.com>
2026-01-06 15:45:05 +0900

    chore/fix-user-domain-setting

--- src/main/java/com/meomulm/user/controller/AuthController.java
+++ src/main/java/com/meomulm/user/controller/AuthController.java
@@ -1,4 +1,92 @@
 package com.meomulm.user.controller;
 
+import com.meomulm.common.util.JwtUtil;
+import com.meomulm.user.model.dto.LoginRequest;
+import com.meomulm.user.model.dto.LoginResponse;
+import com.meomulm.user.model.dto.User;
+import com.meomulm.user.model.service.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+@RestController
 public class AuthController {
+    // ==========================================
+    //               Signup / Login
+    // ==========================================
+    private final UserServiceImpl userService;
+    private final JwtUtil jwtUtil;
+
+    /**
+     * 회원가입
+     * @param user 회원가입할 회원 정보
+     */
+    @PostMapping("/signup")
+    public void signupUser(@RequestBody User user) {
+        userService.signupUser(user);
+    }
+
+    /**
+     * 로그인
+     * @param request 로그인할 유저의 정보
+     * @return
+     */
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> userLogin(@RequestBody LoginRequest request) {
+        try{
+            User user = userService.userLogin(request.getUserEmail(), request.getUserPassword());
+
+            if (user == null) {
+                return ResponseEntity.status(401).body(null);
+            }
+
+            String token = jwtUtil.generateToken(user.getUserId(), user.getUserEmail());
+            LoginResponse loginResponse = new LoginResponse();
+            loginResponse.setToken(token);
+
+            log.info("✅ 로그인 성공 - 이메일 : {}", user.getUserEmail());
+            return ResponseEntity.ok(loginResponse);
+
+        }catch(Exception e) {
+            log.error("❌ 로그인 실패 - 이메일 : {}", e.getMessage());
+            return ResponseEntity.status(401).body(null);
+        }
+    }
+
+    /**
+     * 아이디 찾기
+     * @param userName  회원 이름
+     * @param userPhone 회원 전화번호
+     * @return 회원 이메일
+     */
+    @GetMapping("/findId")
+    public String getUserFindId(@RequestParam String userName,@RequestParam String userPhone){
+        return userService.getUserFindId(userName, userPhone);
+    }
+
+    /**
+     * 비밀번호 찾기
+     * @param userEmail 회원 이메일
+     * @param userBirth 회원 생년
+     * @return 회원 id
+     */
+    @GetMapping("/checkPassword")
+    public Integer getUserFindPassword(@RequestParam String userEmail,@RequestParam String userBirth) {
+        return userService.getUserFindPassword(userEmail, userBirth);
+    }
+
+    /**
+     * 비밀번호 변경 (로그인페이지)
+     * @param userId        변경할 회원 id
+     * @param newPassword   변경할 새로운 비밀번호
+     */
+    @PatchMapping()
+    public void patchUserPassword(Long userId, String newPassword){
+        userService.patchUserPassword(userId, newPassword);
+    }
+
 }
--- src/main/java/com/meomulm/user/model/mapper/UserMapper.java
+++ src/main/java/com/meomulm/user/model/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.meomulm.user.model.mapper;
 
 import com.meomulm.reservation.model.dto.Reservation;
 import com.meomulm.review.model.dto.MyReview;
+import com.meomulm.user.model.dto.LoginRequest;
 import com.meomulm.user.model.dto.User;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -10,25 +11,21 @@ import java.util.List;
 
 @Mapper
 public interface UserMapper {
+    // ==========================================
+    //                  My Page
+    // ==========================================
     /**
-     * 회원정보 조회 메서드
+     * 회원정보 조회
      * @param userId JWT 토큰에서 추출한 userId
      * @return 회원정보
      */
     User selectUserInfoById(int userId);
 
     /**
-     * 회원정보 수정 메서드
-     * @param user
+     * 회원정보 수정
+     * @param user 수정하려는 회원 정보
      */
     void updateUserInfo(User user);
-
-    /**
-     * 회원 리뷰 조회
-     * @param userId JWT 토큰에서 추출한 userId
-     * @return 숙소 정보를 포함한 리뷰 리스트
-     */
-    List<MyReview> selectUserReviewsById(int userId);
 
     /**
      * 회원 예약 내역 조회
@@ -39,8 +36,76 @@ public interface UserMapper {
 
     /**
      * 프로필 사진 수정
-     * @param user 프로필 사진이 담긴 user 객체
+     * @param userProfileImage 회원 프로필 이미지 경로
      */
-    void updateProfileImage(User user);
+    void updateProfileImage(String userProfileImage, int userId);
 
+    /**
+     * 현재 비밀번호 확인
+     * @param userId JWT 토큰에서 추출한 userId
+     * @return 해당 회원의 비밀번호
+     */
+    String selectCurrentPassword(int userId);
+
+    /**
+     * 비밀번호 수정 (마이페이지)
+     * @param userId JWT 토큰에서 추출한 userId
+     * @param userPassword 변경하려는 비밀번호
+     */
+    void updateMyPagePassword(int userId, String userPassword);
+
+    // ==========================================
+    //               Signup / Login
+    // ==========================================
+    /**
+     * 회원가입
+     * @param user 회원 정보
+     */
+    void insertUser(User user);
+
+    /**
+     * 로그인
+     * @param userEmail 로그인하려는 회원 이메일
+     * @return 로그인 성공 여부
+     */
+    LoginRequest selectUserLogin(String userEmail);
+
+    /**
+     * 아이디 찾기
+     * @param userName 아이디를 찾으려는 회원 이름
+     * @param userPhone 아이디를 찾으려는 회원 전화번호
+     * @return 해당 회원의 이메일
+     */
+    String selectUserFindId(String userName, String userPhone);
+
+    /**
+     * 비밀번호 찾기
+     * @param userEmail 회원 이메일 정보
+     * @param userBirth 회원 생일 정보
+     * @return 회원 ID 정보
+     */
+    Integer selectUserFindPassword(String userEmail, String userBirth);
+
+    /**
+     * 비밀번호 번경 (로그인페이지)
+     * @param userId 조회되는 회원 ID
+     * @param newPassword 수정하는 회원 비밀번호
+     * @return 변경 성공 여부
+     */
+    int updateUserPassword(Long userId,String newPassword);
+
+    // 아래는 수정사항에 필요한 조회에 대한 기능
+    /**
+     * 이메일 조회
+     * @param userEmail 회원 이메일
+     * @return
+     */
+    LoginRequest selectUserByUserEmail(String userEmail);
+
+    /**
+     * 전화번호 조회
+     * @param userPhone 회원 전화번호
+     * @return
+     */
+    User selectUserByUserPhone(String userPhone);
 }
