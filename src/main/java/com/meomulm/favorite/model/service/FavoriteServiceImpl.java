72f32090554023dce6322432b8b5f46d3034a3de
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2026-01-08 16:58:22 +0900

    refactor/refactor/favorite-serviceImpl-thrownew-update

--- src/main/java/com/meomulm/favorite/model/service/FavoriteServiceImpl.java
+++ src/main/java/com/meomulm/favorite/model/service/FavoriteServiceImpl.java
@@ -1,5 +1,6 @@
 package com.meomulm.favorite.model.service;
 
+import com.meomulm.common.exception.NotFoundException;
 import com.meomulm.favorite.model.dto.Favorite;
 import com.meomulm.favorite.model.dto.SelectFavorite;
 import com.meomulm.favorite.model.mapper.FavoriteMapper;
@@ -19,37 +20,30 @@ public class FavoriteServiceImpl implements FavoriteService {
 
     @Override
     public List<SelectFavorite> getFavorites(int userId){
-        try {
             List<SelectFavorite> favorites = favoriteMapper.selectFavorites(userId);
             log.info("서비스에서 가져온 데이터들 : {}", favorites);
-            return favorites;
-        } catch (Exception e) {
-            throw new RuntimeException("즐겨찾기 목록을 가져오던 중 오류 발생 : : ", e);
+        if (favorites == null || favorites.isEmpty()) {
+            log.info("서비스에서 가져온 데이터들 : {}", favorites);
+            throw new NotFoundException("사용자의 즐겨찾기 목록이 없습니다.");
         }
+            return favorites;
     }
 
 
 
     @Override
     public void postFavorite(int userId, int accommodationId){
-        try {
             Favorite favorite = new Favorite();
             favorite.setUserId(userId);
             favorite.setAccommodationId(accommodationId);
             favoriteMapper.insertFavorite(favorite);
-        } catch (Exception e) {
-            log.error("즐겨찾기 추가 중 오류 발생 : {}", e.getMessage());
-            throw new RuntimeException("즐겨찾기 추가 중 오류 발생 : ", e);
-        }
+
+
     }
 
     @Override
     public void deleteFavorite(int userId, int favoriteId){
-        try {
             favoriteMapper.deleteFavorite(userId, favoriteId);
-        } catch (Exception e) {
-            log.error("즐겨찾기 삭제 중 오류 발생 : {}", e.getMessage());
-            throw new RuntimeException("즐겨찾기 삭제 중 오류 발생 :", e);
-        }
+
     }
 }
