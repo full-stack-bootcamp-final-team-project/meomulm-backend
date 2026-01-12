2c8a5caf1a444f2dcb1d9ca2c9f5e53361852b0d
svv0003 <ssetoo4689@naver.com>
2026-01-12 16:27:37 +0900

    1/12 pr/merge

--- src/main/java/com/meomulm/reservation/model/service/ReservationServiceImpl.java
+++ src/main/java/com/meomulm/reservation/model/service/ReservationServiceImpl.java
@@ -26,6 +26,7 @@ public class ReservationServiceImpl implements ReservationService {
         return str == null || str.trim().isEmpty();
     }
 
+
     private boolean isValidEmail(String email) {
         if (email == null) return false;
         String regex = "^[A-Za-z0-9+_.-]+@([A-Za-z0-9.-]+\\.[A-Za-z]{2,})$";
