deb483161c8712797269b4f7c28cde9ed92e7acd
svv0003 <ssetoo4689@naver.com>
2026-02-06 15:43:35 +0900

    dateTime

--- src/main/java/com/meomulm/notification/model/dto/Notification.java
+++ src/main/java/com/meomulm/notification/model/dto/Notification.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,6 +16,6 @@ public class Notification {
     private String notificationContent;
     private String notificationLinkUrl;
     private boolean isRead;
-    private String createdAt;
+    private LocalDateTime createdAt;
 
 }
